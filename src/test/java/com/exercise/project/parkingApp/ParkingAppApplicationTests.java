2a6a6f8fe49afeeadce55e4967b262110ac41e10
y-majid16 <yaumulmajid@gmail.com>
2023-08-09 10:37:07 +0700

    first commit

---
+++ src/test/java/com/exercise/project/parkingApp/ParkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.project.parkingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
